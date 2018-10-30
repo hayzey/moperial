41843b12f826d374fc708acfcd18785c32f4e3eb
hayzey <hayzeety@gmail.com>
2018-10-30 14:50:41 +0700

    Wrapping mopidy as an angular service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import Mopidy from 'mopidy';
 
 import { AppComponent } from './app.component';
 
+const mopidy = new Mopidy({
+    webSocketUrl: 'ws://localhost:6680/mopidy/ws',
+});
+
+// Make instance available through developer console
+(<any>window).mopidy = mopidy;
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [
+        { provide: 'Mopidy', useValue: mopidy }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
